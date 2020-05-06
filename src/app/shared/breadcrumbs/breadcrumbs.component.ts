87cf5c2828150e1d544a120d9ebe0de4cabc4768
Nelson <n.sanchezesparza@gmail.com>
2020-05-05 21:42:32 -0500

    Revert "Sección 8: Observable y Promesas"
    This reverts commit 1aba61a4ce335130f84dfa4ed410b641b67ded58.

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,40 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivationEnd } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
-import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: [],
+  styles: [
+  ]
 })
 export class BreadcrumbsComponent implements OnInit {
-  titulo: string;
-  constructor(
-    private router: Router,
-    private title: Title,
-    private meta: Meta
-  ) {
-    this.getDataRouter().subscribe((data) => {
-      this.titulo = data.titulo;
-      this.title.setTitle(this.titulo);
 
-      const metaTag: MetaDefinition = {
-        name: 'description',
-        content: this.titulo,
-      };
+  constructor() { }
 
-      this.meta.updateTag(metaTag);
-    });
+  ngOnInit(): void {
   }
 
-  ngOnInit(): void {}
-
-  getDataRouter() {
-    return this.router.events.pipe(
-      filter((evento) => evento instanceof ActivationEnd),
-      filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
-      map((evento) => evento.snapshot.data)
-    );
-  }
 }
